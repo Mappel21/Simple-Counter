7a0244c4b785cb67d3f27fe9536a8128455310ca
Mappel21 <Mappel7676@gmail.com>
2021-07-21 22:31:57 +0000

    final-count fixed

--- src/js/index.js
+++ src/js/index.js
@@ -18,17 +18,18 @@ function SimpleCounter(props) {
 			<div className="calendar">
 				<i class="far fa-clock"></i>
 			</div>
-			<div className="four">{props.digitFour}</div>
-			<div className="three">{props.digitThree}</div>
-			<div className="two">{props.digitTwo}</div>
-			<div className="one">{props.digitOne}</div>
-		</div>);
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
+		</div>
+	);
 }
 SimpleCounter.propTypes = {
-    digitFour: PropTypes.number,
-    digitThree: PropTypes.number,
-    digitTwo: PropTypes.number,
-    digitOne: PropTypes.number,
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
 };
 
 let counter = 0;
@@ -37,6 +38,16 @@ setInterval(function() {
 	const three = Math.floor(counter / 100);
 	const two = Math.floor(counter / 10);
 	const one = Math.floor(counter / 1);
-	//render your react application
-	ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+	console.log(four, three, two, one);
+	counter++;
+
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
 }, 1000);
